69bf067108ffcaa582eb0843c13748ac90c0863a
SultanArsalan <syedarsalanali8179@gmail.com>
2026-07-27 10:40:46 +0530

    Added my-first-react-app

---
+++ React.js/my-first-react-app/src/student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>
+      <p>Name: Syed Arsalan Ali</p>
+      <p>Course: CSE</p>
+    </div>
+  );
+}
+
+export default Student;
